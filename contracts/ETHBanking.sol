bc8c3d4ad3ab7b1b33c2682f1fa9e472f7d01d32
0x6775737461766f <gt.giacon@gmail.com>
2022-04-19 22:41:11 -0300

    Updated to pragma 0.8.1

--- contracts/ETHBanking.sol
+++ contracts/ETHBanking.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.5.1;
+pragma solidity ^0.8.1;
 
 //This contract uses a "Timelock" function, please note that if you actually use it, you CANNOT withdraw funds until the set date!!!!
 
 contract Banking {
 
-    address payable favored; //Address that is going to receive the ETH.
-    uint256 withdrawDate; //Time - UnixTimeStamp
+    address payable public favored; //Address that is going to receive the ETH.
+    uint256 public withdrawDate; //Time - UnixTimeStamp
 
-    constructor(address payable _favored, uint256 _withdrawDate) public payable {
+    constructor(address payable _favored, uint256 _withdrawDate) payable {
         require(_withdrawDate > block.timestamp);
         favored = _favored;
         withdrawDate = _withdrawDate;
@@ -17,6 +17,6 @@ contract Banking {
 
     function Withdraw() public {
         require(block.timestamp >= withdrawDate);
-        address (favored).transfer(address(this).balance);
+        payable (favored).transfer(address(this).balance);
     }
 }
